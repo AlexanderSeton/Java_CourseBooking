a6ed1abbe6f2b4d5d79c7e4514a1f6eb36b50d61
Craig Marshall <dev.craigmarshall@gmail.com>
2022-01-20 13:17:47 +0000

    booking drivenquery working

--- src/main/java/com/codeclan/example/CourseBookingSystem/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/CourseBookingSystem/controllers/BookingController.java
@@ -1,4 +1,42 @@
 package com.codeclan.example.CourseBookingSystem.controllers;
 
+import com.codeclan.example.CourseBookingSystem.models.Booking;
+import com.codeclan.example.CourseBookingSystem.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> findBookingsFilterByDate(
+            @RequestParam(name = "date", required = false) String date
+    ){
+        if (date != null) {
+            return new ResponseEntity<>(bookingRepository.findBookingsByDate(LocalDate.parse(date)), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(bookingRepository.findAll(),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(value = "/bookings/{id}")
+    public ResponseEntity<Optional<Booking>> getSpecificDistillery(@PathVariable Long id){
+        Optional<Booking> payload = bookingRepository.findById(id);
+        if (payload.isPresent()){
+            return new ResponseEntity<>(payload, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/codeclan/example/CourseBookingSystem/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/CourseBookingSystem/repositories/BookingRepository.java
@@ -4,6 +4,12 @@ import com.codeclan.example.CourseBookingSystem.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findBookingsByDate(LocalDate date);
+
 }
